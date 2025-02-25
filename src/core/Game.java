6a9710b3a9001af3738c16134d63d836ff83aab3
rian-fuzinelli <riancaiofuzinelli12@gmail.com>
2025-02-25 00:08:27 -0300

    adding some classes and methods

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,3 +1,7 @@
+package core;
+
+import core.GamePanel;
+
 import javax.swing.*;
 
 public class Game {
@@ -9,5 +13,11 @@ public class Game {
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel flappyBird = new GamePanel();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
     }
 }
